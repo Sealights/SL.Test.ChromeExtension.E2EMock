6bbeb46c64ef86b0fd213d9444d9654879b0e9a5
yuval ben arie <yuval.ben-arie@sealights.io>
2022-02-13 11:14:14 +0200

    added a test file

---
+++ test.ts
@@ -0,0 +1,10 @@
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+const devide = (a: number, b: number) => {
+    return a / b;
+}
+
+console.log(sum(1, 4));
+console.log(devide(8, 4));
